7f4da5690f75857e4d95bf4ab691911e3ce3141c
Bruce Hsu <1033684650@qq.com>
2024-05-08 13:42:27 +0800

    finish the tabBarItem

--- Tinder/ContentView.swift
+++ Tinder/ContentView.swift
@@ -1,16 +1,48 @@
-//
-//  ContentView.swift
-//  Tinder
-//
-//  Created by mac on 2024/5/8.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    // 导航栏数组
+    var tabs: [TabBarItem] = [
+        TabBarItem(title: "Home", icon: "rectangle.fill.on.rectangle.fill"),
+        TabBarItem(title: "Live", icon: "play.tv.fill"),
+        TabBarItem(title: "Message", icon: "message.fill"),
+        TabBarItem(title: "Profile", icon: "person.fill")
+    ]
+    
+    // 记住用户的选择
+    @State var selection: TabBarItem = TabBarItem(title: "Home", icon: "rectangle.fill.on.rectangle.fill")
+    
     var body: some View {
-        Text("Hello Bruce")
+        VStack{
+            Spacer()    // 占用最大的空间
+            HStack {
+                ForEach(tabs, id: \.self) { tab in
+                    createTabBarItem(tabBarItem: tab)
+                        .onTapGesture {
+                            selection = tab
+                        }
+                }
+            }
+        }
     }
+    
+    func createTabBarItem(tabBarItem: TabBarItem) -> some View {
+        VStack {
+            Image(systemName: tabBarItem.icon)
+                .font(.system(size: 24, weight: .bold))
+                .foregroundStyle(selection == tabBarItem ? Color.accentColor : Color.gray.opacity(0.5))
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+            Text(tabBarItem.title)
+                .font(.system(size: 10))
+                .foregroundStyle(selection == tabBarItem ? Color.accentColor : Color.gray.opacity(0.5))
+        }
+    }
+}
+
+struct TabBarItem: Hashable {
+    let title: String
+    let icon: String
 }
 
 #Preview {
