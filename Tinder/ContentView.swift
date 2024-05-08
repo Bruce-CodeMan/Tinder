73ad2b53adbb6c66874c88f1a848be0086917e68
Bruce Hsu <1033684650@qq.com>
2024-05-08 11:04:51 +0800

    Initial Commit

---
+++ Tinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tinder
+//
+//  Created by mac on 2024/5/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tinder/TinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderApp.swift
+//  Tinder
+//
+//  Created by mac on 2024/5/8.
+//
+
+import SwiftUI
+
+@main
+struct TinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
