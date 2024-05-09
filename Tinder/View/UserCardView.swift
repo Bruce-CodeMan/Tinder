e4fd4527a5428d198ae26415108fe064a522045e
Bruce Hsu <1033684650@qq.com>
2024-05-09 11:19:03 +0800

    modified the UserCardView style

--- Tinder/View/UserCardView.swift
+++ Tinder/View/UserCardView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct UserCardView: View {
     var userCard: UserCard
     @State var imageIndex = 0
+    @State var offset: CGSize = .zero
     var body: some View {
         // 获取不同手机型号的宽度和高度
         GeometryReader { proxy in
@@ -33,13 +34,69 @@ struct UserCardView: View {
             }
             .padding(.top, 10)
             .padding(.horizontal)
+            HStack {
+                if offset.width > 0 {
+                    createUserCardLabel(title: "LIKE", degree: -20, color: Color.green)
+                    Spacer()
+                }
+                if offset.width < 0 {
+                    Spacer()
+                    createUserCardLabel(title: "NOPE", degree: 20, color: Color.red)
+                }
+            }
+            .padding(.horizontal, 30)
+            .padding(.top, 40)
         }
+        .offset(offset)
+        .scaleEffect(getScaleAmount())
+        .rotationEffect(Angle(degrees: getRotateAmount()))
+        .gesture(
+            DragGesture()
+                .onChanged { value in
+                    offset = value.translation
+                }
+                .onEnded { value in
+                    offset = .zero
+                }
+        )
+        
     }
     
+    // 左右滑动
     func updateImageIndex(hasMoreIndex: Bool) {
         let nextIndex = hasMoreIndex ? imageIndex + 1 : imageIndex - 1
         imageIndex = min(max(0, nextIndex), userCard.photos.count - 1)
         
+    }
+    
+    // 喜欢和不喜欢的标志
+    func createUserCardLabel(title: String, degree: Double, color: Color) -> some View {
+        Text(title)
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            .padding(.horizontal)
+            .foregroundStyle(color)
+            .overlay(
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(color, lineWidth: 3)
+            )
+            .rotationEffect(.degrees(degree))
+    }
+    
+    // 放大缩小
+    func getScaleAmount() -> CGFloat{
+        let max = UIScreen.main.bounds.width / 2
+        let currentAmount = abs(offset.width)
+        let percentage = currentAmount / max
+        return 1.0 - min(percentage, 0.7) * 0.3
+    }
+    
+    // 旋转效果
+    func getRotateAmount() -> Double {
+        let max = UIScreen.main.bounds.width / 2
+        let currentAmount = offset.width
+        let percentage = currentAmount / max
+        return Double(percentage * 10)
     }
 }
 
